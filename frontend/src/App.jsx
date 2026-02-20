ad3daf32216653300e29ca9a178e61c7030d1a1f
uzair tariq <uzairtariqm1@gmail.com>
2026-02-20 12:24:36 +0500

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import Create from "./pages/Create";
+import NoteDetailPage from "./pages/NoteDetailPage";
+import toast from "react-hot-toast";
+
+const App = () => {
+  return (
+    <div>
+      <button
+        onClick={() => toast.error("congrats ")}
+        className="text-white bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded"
+      >
+        Click me
+      </button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/notes/:id" element={<NoteDetailPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/pages/Create.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Create = () => {
+  return <div className="bg-red-500">Create</div>;
+};
+
+export default Create;
