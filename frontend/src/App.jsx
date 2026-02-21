f0948adcde9afe0b0394c57fe356b07ac96d6e65
uzair tariq <uzairtariqm1@gmail.com>
2026-02-21 11:17:26 +0500

    daisyui added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,20 @@ import { Routes, Route } from "react-router";
 import Home from "./pages/Home";
 import Create from "./pages/Create";
 import NoteDetailPage from "./pages/NoteDetailPage";
-import toast from "react-hot-toast";
+// import toast from "react-hot-toast";
 
 const App = () => {
   return (
     <div>
-      <button
+      <div>
+        <button className="btn btn-primary">click me</button>
+      </div>
+      {/* <button
         onClick={() => toast.error("congrats ")}
         className="text-white bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded"
       >
         Click me
-      </button>
+      </button> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<Create />} />
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
 export default {
   content: ["./src/**/*.{html,js,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["forest"],
+  },
 };
